e584ab2f3622c24c154aec3fa052cfd4bdf08771
marie <marieow35@gmail.com>
2024-03-19 00:02:08 -0400

    electric field

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ let maxScalar = 0.88;
 
 //PN Junction Initial Variables
 let spheres = [];
-let cube;
-let cubeSize = 50;
+let cube1, cube2;
+let cubeSize = 75;
 let clock = new THREE.Clock();
 let xLevel = 0.0;
 
@@ -35,9 +35,9 @@ function init() {
 
     //camera
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1500 );
-    camera.position.x = 86;
-    camera.rotation.y = MathUtils.degToRad(38);
-    camera.position.z = 92;
+    // camera.position.x = 86;
+    // camera.rotation.y = MathUtils.degToRad(38);
+    camera.position.z = 116;
     //renderer
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -46,15 +46,15 @@ function init() {
     // GUI
     gui = new dat.GUI();
     cameraControls = {
-        translateZ : 92,
-        translateX: 86,
-        rotateY: MathUtils.degToRad(38),
+        translateZ : 116,
+        translateX: 0,
+        rotateY: MathUtils.degToRad(0),
     };
 
     initialCameraControls = {
-        translateZ : 92,
-        translateX: 86,
-        rotateY: MathUtils.degToRad(38),
+        translateZ : 116,
+        translateX: 0,
+        rotateY: MathUtils.degToRad(0),
     };
 
     electricFieldControl = {
@@ -107,9 +107,17 @@ function init() {
     // scene.background = textureCube;
 
     // create cube container
-    createCube();
+    const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x808080, wireframe: true, transparent: true, opacity: 0.1});
+    cube1 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    //later position should be a param
+    cube1.position.set(-30, 0, 0);
+    cube2.position.set(30, 0, 0);
+    scene.add(cube1, cube2);
+
     // create initial electrons
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 50; i++) {
         createSphere();
     }
 }
@@ -118,9 +126,9 @@ function init() {
 function resetGUI() {
     console.log('reset attempted');
     Object.assign(cameraControls, initialCameraControls, electricFieldControl, initialElectricFieldControl);
-        camera.position.x = 86;
-        camera.rotation.y = MathUtils.degToRad(38);
-        camera.position.z = 92; 
+        camera.position.x = 0;
+        camera.rotation.y = MathUtils.degToRad(0);
+        camera.position.z = 116; 
     gui.updateDisplay(); // Update GUI to reflect the changes
 }
 
@@ -134,31 +142,29 @@ function createSphere() {
   
   // Random position within the cube
   sphere.position.set(
-    THREE.MathUtils.randFloat(-cubeSize/2 + 1, cubeSize/2 - 1),
-    THREE.MathUtils.randFloat(-cubeSize/2 + 1, cubeSize/2 - 1),
-    THREE.MathUtils.randFloat(-cubeSize/2 + 1, cubeSize/2 - 1)
+    THREE.MathUtils.randFloat(-cubeSize/2 + 1, cubeSize/2 - 20),
+    THREE.MathUtils.randFloat(-cubeSize/2 + 1, cubeSize/2 - 20),
+    THREE.MathUtils.randFloat(-cubeSize/2 + 1, cubeSize/2 - 20)
   );
   
-  cube.add(sphere);
+  cube1.add(sphere);
   let randomVelocity = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
   spheres.push({ object: sphere, velocity: randomVelocity,
     speed: Math.random() * (maxScalar - minScalar + 1) + minScalar, initVelocity: randomVelocity});
 }
 
-function createCube() {
-    // Create a cube to contain spheres
-    const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x808080, wireframe: true, transparent: true, opacity: 0.1});
-    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
-}
 
 function update() {
 	requestAnimationFrame( update );
-    let time = clock.getDelta() * 20;
+    // let time = 0.01;
+    // let time = clock.getDelta();
+    // console.log('pre:' + time);
+    let time = clock.getDelta()/15;
+    console.log("post" + time);
     
     //dis my electric field...
     let electricField = new THREE.Vector3(xLevel, 0, 0);
+    let acc = electricField.x;
     
     // scatter every 6 seconds
     if (Date.now() % 6000 < 16.7) {
@@ -173,78 +179,60 @@ function update() {
     }
     // adjust velocity
     spheres.forEach((sphere) => {
-       
-        sphere.velocity.normalize();
-        //electric field check, electrons are constantly increasing
-        if (electricField.length() !== 0) {
-            console.log("time:", time);
-            console.log("pre-xVel:", sphere.velocity.x)
-            // random direction when electric field is active
-            // sphere.velocity = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
-            // // random speed when electric field is active
-            // sphere.speed =Math.random() * (maxScalar - minScalar + 1) + minScalar
-
-            
-
-            // Calculate the increment based on the electric field
-            let incVelocityX = electricField.x * time;
-
-            // Scale the direction vector with the increment
-
-            console.log("post-xVel:", -incVelocityX);
-            //if the sphere velocity is positive then negate new increasing velocity
-            // if (sphere.velocity.x > 0) {
-            sphere.velocity.x = sphere.initVelocity.x - incVelocityX;
-            // } 
+        //if electric field is active
+        const currVelocity = sphere.velocity.clone();
+        if (acc !== 0) {
+            const accVector = new THREE.Vector3(-acc, 0, 0); 
+            currVelocity.add(accVector.multiplyScalar(time));
         }
-        // console.log(sphere.velocity);
-       
+        
+        currVelocity.normalize();
         // randomizes the electron speed
-        sphere.velocity.multiplyScalar(sphere.speed);
+        currVelocity.multiplyScalar(sphere.speed);
        
-
         // Apply a minimum velocity threshold
         const minVelocity = 0.2;
         const maxVelocity = 0.6;
-        sphere.velocity.clampLength(minVelocity, maxVelocity);
-        
-        checkBounds(sphere);
-        sphere.object.position.add(sphere.velocity);
-    });
+        currVelocity.clampLength(minVelocity, maxVelocity);
 
-    
+        sphere.object.position.add(currVelocity);
+        sphere.velocity = currVelocity;
+        checkBounds(sphere);
+    }); 
 	renderer.render( scene, camera );
 }
 
 function checkBounds(sphere) {
     // cube boundaries
-    let edge = (cubeSize/2) - 1;
+    let edge = (cubeSize/2) - 2;
     let nedge = -(edge);
     
-    if (sphere.object.position.x >= edge) {
+    if (sphere.object.position.x >= cubeSize/2 - 20) {
         console.log('sphere greater than x pos edge');
-        sphere.object.position.x = -edge;
-        // sphere.velocity.x *= -1.8;
+        // sphere.object.position.x = -edge;
+        sphere.velocity.x *= -1;
     } else if(sphere.object.position.x <= nedge){
-        sphere.object.position.x = edge;
+        // sphere.object.position.x = edge;
+        sphere.velocity.x *= -1;
     }
 
     if (sphere.object.position.y >= edge) {
         console.log('sphere greater than y pos edge');
         sphere.object.position.y = -edge;
-        // sphere.velocity.y *= -1.8;
+        // sphere.velocity.y *= -1;
     } else if (sphere.object.position.y <= nedge) {
         sphere.object.position.y = edge;
+        // sphere.velocity.y *= -1;
     }
     if (sphere.object.position.z >= edge) {
         console.log('sphere greater than z pos edge');
         sphere.object.position.z = edge;
-        // sphere.velocity.z *= -1.8;
+        // sphere.velocity.z *= -1;
     } else if (sphere.object.position.z <= nedge) {
         sphere.object.position.z = -edge;
+        // sphere.velocity.z *= -1;
     }
 }
-
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
