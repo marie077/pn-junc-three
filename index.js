a5b0c5962125402c19587420a3cf1304e3b865af
marie ow <marieow35@gmail.com>
2025-02-07 13:33:35 -0500

    background

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ function init() {
     container = document.getElementById('three-container-scene-1');
     //scene
     scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x808080);
+	scene.background = new THREE.Color(0x000000);
     //camera
     camera = new THREE.PerspectiveCamera( 75, container.clientWidth / container.clientHeight, 0.1, 1500);
     camera.position.z = 116;
