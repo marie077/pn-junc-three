936ae576daad47ea7fb6c8f7c3228f34c0cf04c4
marie ow <marieow35@gmail.com>
2025-03-04 10:13:00 -0500

    work

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/pn-junc-three/',  // Change to your actual GitHub repository name
+  base: '/pn-junc-three/', // Change this to your actual GitHub repo name
+  build: {
+    rollupOptions: {
+      input: 'index.html',  // Ensures all scripts in index.html are kept
+    },
+  },
 });
