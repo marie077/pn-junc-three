6247078f35b7a355038cec0f5a1e4eb40e3e7be2
marie ow <marieow35@gmail.com>
2025-03-04 11:47:24 -0500

    Fix solarcell.js Rollup issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: `/pn-junc-three/${process.env.BRANCH_NAME || ''}/`,  // Use dynamic branch name
+  publicDir: 'public',
   rollupOptions: {
     input: 'index.html',  // Ensures all scripts in index.html are kept
-  },
+    external: ['/pn-junc-three/solarcell.js'],
+},
 });
