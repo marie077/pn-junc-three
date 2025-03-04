8b69b00d300ccbb818d7d56ae2083d7d15fa07d4
marie ow <marieow35@gmail.com>
2025-03-04 12:08:02 -0500

    Fix double slashes in script paths

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 
+const branchName = process.env.BRANCH_NAME ? `/${process.env.BRANCH_NAME}` : ''; 
+
 export default defineConfig({
-  base: `/pn-junc-three/${process.env.BRANCH_NAME || ''}/`,  // Use dynamic branch name
-  publicDir: 'public',
+    base: `/pn-junc-three${branchName}`,  // Ensure no trailing slash issue
+    publicDir: 'public',
   rollupOptions: {
     input: 'index.html',  // Ensures all scripts in index.html are kept
     external: ['/pn-junc-three/solarcell.js'],
     assetsInclude: ['**/*.hdr'], // Ensure HDR files are included in the build
 },
 });
-
-
-
