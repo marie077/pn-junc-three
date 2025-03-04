9ab05149fbee561e474afeb9b45327e0c739dcab
marie ow <marieow35@gmail.com>
2025-03-04 10:52:20 -0500

    Setup GitHub Pages for multiple branches

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/pn-junc-three/', // Change this to your actual GitHub repo name
-  build: {
-    rollupOptions: {
-      input: 'index.html',  // Ensures all scripts in index.html are kept
-    },
+  base: `/pn-junc-three/${process.env.BRANCH_NAME || ''}/`,  // Use dynamic branch name
+  rollupOptions: {
+    input: 'index.html',  // Ensures all scripts in index.html are kept
   },
 });
